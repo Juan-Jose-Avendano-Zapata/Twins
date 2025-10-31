1e40a5d3bbde67ef1d14430aad03aef9be3411a6
Juan Jose Avendaño Zapata <juan.avendano27@correo.tdea.edu.co>
2025-10-31 09:35:00 -0500

    update: creating component profile view

--- App.js
+++ App.js
@@ -6,6 +6,7 @@ import Main from './src/screens/Main';
 import CreateAccount from './src/screens/CreateAccount';
 import Login from './src/screens/Login';
 import Home from './src/screens/Home';
+import Profile from './src/screens/Profile';
 
 const Stack = createNativeStackNavigator();
 
@@ -13,11 +14,12 @@ export default function App() {
   return (
     <PaperProvider>
       <NavigationContainer>
-        <Stack.Navigator initialRouteName="Login">
+        <Stack.Navigator initialRouteName="Profile">
           <Stack.Screen name="Main" component = {Main} options={{ headerShown: false }}/>
           <Stack.Screen name="CreateAccount" component = {CreateAccount} options={{ headerShown: false }}/>
           <Stack.Screen name="Login" component = {Login} options={{ headerShown: false }}/>
           <Stack.Screen name="Home" component = {Home} options={{ headerShown: false }}/>
+          <Stack.Screen name='Profile' component={Profile} options={{ headerShown: false }} />
         </Stack.Navigator>
       </NavigationContainer>
     </PaperProvider>
